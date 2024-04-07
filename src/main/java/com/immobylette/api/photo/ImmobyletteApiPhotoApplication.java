dbdb26afd4c7b336cec4c5331cf867c88173e2bf
OdakK <julien.2002@yahoo.fr>
2024-04-07 18:09:52 +0200

    devops: Create base project

---
+++ src/main/java/com/immobylette/api/photo/ImmobyletteApiPhotoApplication.java
@@ -0,0 +1,13 @@
+package com.immobylette.api.photo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmobyletteApiPhotoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmobyletteApiPhotoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/immobylette/api/photo/ImmobyletteApiPhotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immobylette.api.photo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmobyletteApiPhotoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
