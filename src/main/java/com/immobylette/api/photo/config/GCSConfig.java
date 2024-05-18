56d7f4bcd4623c04340c552371b1f927619f304a
LfrPaul <78427088+LfrPaul@users.noreply.github.com>
2024-05-18 15:17:47 +0200

    fix/IMB-98
    [FIX] Use ClassPathResource in GCSConfig

--- src/main/java/com/immobylette/api/photo/config/GCSConfig.java
+++ src/main/java/com/immobylette/api/photo/config/GCSConfig.java
@@ -4,14 +4,16 @@ import com.google.auth.Credentials;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import org.springframework.core.io.ClassPathResource;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+
+import org.springframework.core.io.Resource;
 
 @Configuration
 @ConfigurationProperties(prefix = "gcs")
@@ -28,8 +30,10 @@ public class GCSConfig {
 
     @Bean
     public Storage GCSStorage() throws IOException {
+        Resource resource = new ClassPathResource(this.credentialsLocation);
+
         Credentials credentials = GoogleCredentials
-                .fromStream(new FileInputStream(credentialsLocation));
+                .fromStream(resource.getInputStream());
 
         return StorageOptions.newBuilder().setCredentials(credentials).setProjectId(projectId).build().getService();
     }
